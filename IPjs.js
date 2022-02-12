fa11d25b8f146d02882fbab81b6644378aad0ed2
ng-shijun-jess <S10222791@connect.np.edu.sg>
2022-02-12 15:09:50 +0800

    cconnected jquery and able to make tab responsive

--- IPjs.js
+++ IPjs.js
@@ -1,3 +1,13 @@
 $('.nav ul li').click(function() {
-    $(this).addClass('active').siblings().removeClass('active');
+    $(this).addClass("active").siblings().removeClass("active");
 })
+
+const tabBtn = document.querySelectorAll('.nav ul li');
+const tab = document.querySelectorAll('.tab');
+function tabs(panelIndex){
+    tab.forEach(function(node){
+        node.style.display = 'none';
+    });
+    tab[panelIndex].style.display  = 'block';
+}
+tabs(0);
