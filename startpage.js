9b9f736ebdf694cc4fb6f4ef2bc3d1bdc3923aa8
Guo Ji Xuan <92961123+InanimateJR@users.noreply.github.com>
2022-02-17 10:33:59 +0800

    start page finished but not responsive when small
    page is unresponsive when width is below 1496px. API has not been implemented yet

---
+++ startpage.js
@@ -0,0 +1,8 @@
+var rotatebox = document.getElementById("rotatebox");
+
+function openLogin(){
+    rotatebox.style.transform="rotateY(0deg)";
+}
+function openRegister(){
+    rotatebox.style.transform="rotateY(-180deg)";
+}
