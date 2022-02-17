3c52bfbdf66a91ec1a816a43122e40c4fe32aeec
Guo Ji Xuan <92961123+InanimateJR@users.noreply.github.com>
2022-02-17 12:38:49 +0800

    Put lottie animations on Feed and startpage
    All relevant sections have been commented and are ready to be compiled (css)

--- startpage.js
+++ startpage.js
@@ -1,3 +1,4 @@
+// Rotates .button_box to swap between login and register
 var rotatebox = document.getElementById("rotatebox");
 
 function openLogin(){
@@ -6,3 +7,14 @@ function openLogin(){
 function openRegister(){
     rotatebox.style.transform="rotateY(-180deg)";
 }
+
+// Lottie animation disappears on page load
+let lottie_container = document.querySelector(".lottie_container")
+let lottie_loading = document.querySelector(".lottie_loading")
+
+window.addEventListener("load", function(){
+    lottie_container.style.display = 'none';
+    lottie_container.style.opacity="0";
+    lottie_loading.style.display = "none";
+    lottie_loading.style.opacity="0";
+})
