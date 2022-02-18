91eeaa2a9bf03deb508ae5b16eb8aa4e9bf3f5a6
ng-shijun-jess <S10222791@connect.np.edu.sg>
2022-02-18 12:42:59 +0800

    frontend

---
+++ frontend/IPjs.js
@@ -0,0 +1,23 @@
+$('.nav ul li').click(function() {
+    $(this).addClass("active").siblings().removeClass("active");
+})
+
+const tabBtn = document.querySelectorAll('.nav ul li');
+const tab = document.querySelectorAll('.tab');
+function tabs(panelIndex){
+    tab.forEach(function(node){
+        node.style.display = 'none';
+    });
+    tab[panelIndex].style.display  = 'block';
+}
+tabs(0);
+
+function increment() {
+    document.getElementById('demoInput').stepUp();
+ }
+ function decrement() {
+    document.getElementById('demoInput').stepDown();
+ }
+
+
+ 
